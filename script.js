b9ad7aeab64725f3b84e537176bb1a23d7e2e191
NAZMUS SAKIB <sakibabir52@gmail.com>
2021-02-07 20:21:02 +0600

    Hungry Monster rebuild

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// const searchMeal = document.getElementById('search-meal');
-// const inputMeal = document.getElementById('input-meal');
 const mealResults = document.getElementById('meal-results');
 
 
@@ -10,16 +8,13 @@ const searchBtn = document.getElementById('search-meal').addEventListener('click
     // console.log(inputMeal.value);
     if (input == '' || input == null) {
         alert('Please enter a Meal name');
-    } 
-    else {
+    } else {
         const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`;
         fetch(url)
             .then(res => res.json())
             .then(data => {
-                // console.log(data);
-                console.log(data.meals[0]);
                 mealResults.innerHTML = data.meals.map(meal => `
-                        <div class="meal-div">
+                        <div onclick="displayMealInfo('${meal.strMeal}')" class="meal-div">
                             <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
                             <h4 class="meal-name">${meal.strMeal}</h4>
                         </div>
@@ -29,29 +24,28 @@ const searchBtn = document.getElementById('search-meal').addEventListener('click
         // Clear search text
         inputMeal.value = '';
     }
-
 })
 
 
+const displayMealInfo = name => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => renderMealInfo(data.meals[0]));
+};
 
 
-// Meal result show
-// const getMeal = name => {
-//     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(data => displayMeals(data.meals[0]));
-// }
-
-
-// const displayMeals = meals => {
-//     // console.log(meals.strMeal);
-
-//     const mealResults = document.getElementById('meal-results');
-//     mealResults.innerHTML = `
-//         <div class="meal-div">
-//             <img src="${meals.strMealThumb}">
-//             <h4>${meals.strMeal}</h4>
-//         </div>
-//     `;
-// }
+const renderMealInfo = meals => {
+    const mealInfo = document.getElementById('meal-info');
+    mealInfo.innerHTML = `
+        <img src="${meals.strMealThumb}">
+        <h2>${meals.strMeal}</h2>
+        <h4>Ingredients</h4>
+        <p>${meals.strIngredient1}</p>
+        <p>${meals.strIngredient2}</p>
+        <p>${meals.strIngredient3}</p>
+        <p>${meals.strIngredient4}</p>
+        <p>${meals.strIngredient5}</p>
+        <p>${meals.strIngredient6}</p>
+    `
+}
