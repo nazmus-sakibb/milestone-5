5386c86142b79796e75a312475b34ac0d5d834e2
NAZMUS SAKIB <sakibabir52@gmail.com>
2021-02-07 19:05:05 +0600

    hungry monster is cooking foods for you

--- script.js
+++ script.js
@@ -1,31 +1,57 @@
-const searchMeal = document.getElementById('search-meal');
-const inputMeal = document.getElementById('input-meal');
+// const searchMeal = document.getElementById('search-meal');
+// const inputMeal = document.getElementById('input-meal');
 const mealResults = document.getElementById('meal-results');
 
-// Event listener
-searchMeal.addEventListener('click', getInput);
 
-function getInput(){
+// Search meal
+const searchBtn = document.getElementById('search-meal').addEventListener('click', function () {
+    const inputMeal = document.getElementById('input-meal');
     const input = inputMeal.value;
-
-    //Empty value check
-    if(input == '' || input == Number){
+    // console.log(inputMeal.value);
+    if (input == '' || input == null) {
         alert('Please enter a Meal name');
-    }
-    else{
-        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            mealResults.innerHTML = data.meals.map(meal => `
-                <div class="mealDiv">
-                    <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-                    <h4 class="meal-name">${meal.strMeal}</h4>
-                </div>
-            `)
-            .join('');
-        });
+    } 
+    else {
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                console.log(data.meals[0]);
+                mealResults.innerHTML = data.meals.map(meal => `
+                        <div class="meal-div">
+                            <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+                            <h4 class="meal-name">${meal.strMeal}</h4>
+                        </div>
+                    `)
+                    .join('');
+            });
         // Clear search text
         inputMeal.value = '';
     }
-}
+
+})
+
+
+
+
+// Meal result show
+// const getMeal = name => {
+//     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`;
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => displayMeals(data.meals[0]));
+// }
+
+
+// const displayMeals = meals => {
+//     // console.log(meals.strMeal);
+
+//     const mealResults = document.getElementById('meal-results');
+//     mealResults.innerHTML = `
+//         <div class="meal-div">
+//             <img src="${meals.strMealThumb}">
+//             <h4>${meals.strMeal}</h4>
+//         </div>
+//     `;
+// }
